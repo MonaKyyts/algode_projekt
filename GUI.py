0de40d62c2e83894f5986cf8690524a4c438e638
TiiratsT <timo.tiirats@gmail.com>
2021-01-19 17:35:22 +0200

    Adding pseudocode for finding and deleteing.

--- GUI.py
+++ GUI.py
@@ -42,6 +42,8 @@ def draw_line(canvas, x_start, y_start, x_end, y_end):
 
 
 def canvas_clear_all(canvas):
+    global root_node
+    root_node = None
     canvas.delete('all')
 
 
@@ -51,63 +53,47 @@ def insert_new_node(canvas):
     i = int(number_input.get())  # Gets number from input field
     root_node = myTree.insert(root_node, i) # Adds the number to AVL-tree
     print(root_node.to_string())
-    canvas_clear_all(canvas) # Clear canvas to start drawing again
+    canvas.delete('all') # Clear canvas to start drawing again
     draw_tree_recursion(canvas, root_node, starting_coordinates[0], starting_coordinates[1], 0)  # Start drawing
     #except:
     #    messagebox.showerror("Value error", "Please enter a number!")
     number_textbox.delete(0, 'end')
 
 
-def draw_example_tree_by_hand(canvas):
-    # Node 1
-    draw_circle(canvas, 700, 20, "10")
-    draw_line(canvas, 700, 20, 1000, 90)
-    draw_line(canvas, 700, 20, 400, 90)
+def find_node():
+    #global root_node
+    #i = int(number_input.get())
+    #if (AVLTree has node):
+    #   messagebox.showinfo("Is {} in tree".format(i), "Yes!")
+    #else:
+    #   messagebox.showinfo("Is {} in tree".format(i), "No!")
+    #number_textbox.delete(0, 'end')
+    return
 
-    # Node 2
-    draw_circle(canvas, 400, 90, "7")
-    draw_line(canvas, 400, 90, 470, 160)
-    draw_line(canvas, 400, 90, 330, 160)
 
-    # Node 3
-    draw_circle(canvas, 1000, 90, "12")
-    draw_line(canvas, 1000, 90, 1070, 160)
-    draw_line(canvas, 1000, 90, 930, 160)
-
-    # Node 4
-    draw_circle(canvas, 330, 160, "3")
-
-    # Node 5
-    draw_circle(canvas, 470, 160, "9")
-    draw_line(canvas, 470, 160, 400, 230)
-
-    # Node 6
-    draw_circle(canvas, 400, 230, "8")
-
-    # Node 7
-    draw_circle(canvas, 1070, 160, "15")
-
-    # Node 8
-    draw_circle(canvas, 930, 160, "11")
+def delete_node(canvas):
+    # global root_node
+    # i = int(number_input.get())  # Gets number from input field
+    # root_node = myTree.delete(root_node, i)  # Adds the number to AVL-tree
+    # canvas.delete('all')  # Clear canvas to start drawing again
+    # draw_tree_recursion(canvas, root_node, starting_coordinates[0], starting_coordinates[1], 0)  # Start drawing
+    # number_textbox.delete(0, 'end')
+    return
 
 
 def draw_tree_recursion(canvas, node, start_x, start_y, layer_nr):
     # draw circle
     draw_circle(canvas, start_x, start_y, str(node.value))
 
-    # if not (node.left_child or node.right_child)
-    #     return
-
     # draw left child and line
     if node.left != None:
-         draw_line(canvas, start_x, start_y, start_x-x_gap_per_layer[layer_nr], start_y+y_gap_per_layer)
-         draw_tree_recursion(canvas, node.left, start_x-x_gap_per_layer[layer_nr], start_y+y_gap_per_layer, layer_nr+1)
+         draw_line(canvas, start_x, start_y, start_x-x_gap_per_layer[min(layer_nr, len(x_gap_per_layer)-1)], start_y+y_gap_per_layer)
+         draw_tree_recursion(canvas, node.left, start_x-x_gap_per_layer[min(layer_nr, len(x_gap_per_layer)-1)], start_y+y_gap_per_layer, layer_nr+1)
 
     # draw right child and line
     if node.right != None:
-         draw_line(canvas, start_x, start_y, start_x+x_gap_per_layer[layer_nr], start_y+y_gap_per_layer)
-         draw_tree_recursion(canvas, node.right, start_x+x_gap_per_layer[layer_nr], start_y+y_gap_per_layer, layer_nr+1)
-
+         draw_line(canvas, start_x, start_y, start_x+x_gap_per_layer[min(layer_nr, len(x_gap_per_layer)-1)], start_y+y_gap_per_layer)
+         draw_tree_recursion(canvas, node.right, start_x+x_gap_per_layer[min(layer_nr, len(x_gap_per_layer)-1)], start_y+y_gap_per_layer, layer_nr+1)
 
 
 ################################
@@ -136,11 +122,14 @@ number_textbox.pack(side=LEFT, pady=4, padx=4)
 insert_button = Button(top_frame, text="Insert", command= lambda: insert_new_node(canvas))
 insert_button.pack(side=LEFT, padx=4)
 
+find_button = Button(top_frame, text="Find", command= lambda: find_node())
+find_button.pack(side=LEFT, padx=4)
+
+del_button = Button(top_frame, text="Delete", command= lambda: delete_node(canvas))
+del_button.pack(side=LEFT, padx=4)
+
 clear_button = Button(top_frame, text="Clear", command= lambda: canvas_clear_all(canvas))
 clear_button.pack(side=LEFT, padx=4)
-
-example_button = Button(top_frame, text="Example", command= lambda: draw_example_tree_by_hand(canvas))
-example_button.pack(side=LEFT, padx=4)
 
 # Run GUI
 root.mainloop()
